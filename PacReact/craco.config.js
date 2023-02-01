6e017181d3f1d3da2ae8ff8c964185201c34bed7
P-Nth <philemonondigo@protonmail.com>
2023-02-01 15:24:28 +0300

    Initial commit

--- PacReact/craco.config.js
+++ PacReact/craco.config.js
@@ -3,23 +3,13 @@ const path = require("path");
 module.exports = {
   webpack: {
     alias: {
-      "@components": path.resolve(__dirname, "src/components/"),
-      "@context": path.resolve(__dirname, "src/context/"),
-      "@styles": path.resolve(__dirname, "src/styles/"),
-      "@routes": path.resolve(__dirname, "src/routes/"),
-      "@pages": path.resolve(__dirname, "src/pages/"),
-      "@hooks": path.resolve(__dirname, "src/hooks/"),
+      "@components": path.resolve(__dirname, "src/components/")
     },
   },
   jest: {
     configure: {
       moduleNameMapper: {
-        "^@components(.*)$": "<rootDir>/src/components$1",
-        "^@context(.*)$": "<rootDir>/src/context$1",
-        "^@styles(.*)$": "<rootDir>/src/styles$1",
-        "^@routes(.*)$": "<rootDir>/src/routes$1",
-        "^@pages(.*)$": "<rootDir>/src/pages$1",
-        "^@hooks(.*)$": "<rootDir>/src/hooks$1",
+        "^@components(.*)$": "<rootDir>/src/components$1"
       },
     },
   },
